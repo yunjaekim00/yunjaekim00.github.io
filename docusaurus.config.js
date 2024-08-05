9fd8ea22fdc9797ed630f1cefb90423dd446a7ff
Yun Jae Kim <82442974+yunjaekim00@users.noreply.github.com>
2024-08-05 15:43:33 +0900

    test

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,7 +22,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'yunjaekim00', // Usually your GitHub org/user name.
   projectName: 'yunjaekim00.github.io', // Usually your repo name.
-
+  trailingSlash: false,
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
