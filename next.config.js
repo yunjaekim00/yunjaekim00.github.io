8366001fe639f6f838d96b36599fd3feee94fb16
Yun Jae Kim <82442974+yunjaekim00@users.noreply.github.com>
2024-09-14 16:47:52 +0900

    modify structure

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 const withNextra = require("nextra")({
 	theme: "nextra-theme-docs",
 	themeConfig: "./theme.config.jsx",
@@ -8,9 +10,30 @@ const nextConfig = {
 		unoptimized: true,
 	},
 	reactStrictMode: false,
-}
+};
+
+//const excludeBuild = {
+//	webpack: (config) => {
+//		config.module.rules.push({
+//			test: /\.md$/,
+//			use: 'null-loader', // Ignore Markdown files
+//		});
+
+//		config.module.rules.push({
+//			test: /\.js$/,
+//			include: [
+//				path.resolve(__dirname, `/.obsidian`), // Move .obsidian outside of pages/
+//				path.resolve(__dirname, `/assets`), // Move assets outside of pages/
+//			],
+//			use: 'null-loader', // Ignore files inside src/obsidian and src/assets/templates
+//		});
+
+//		return config;
+//	},
+//};
 
 module.exports = {
 	...withNextra(),
 	...nextConfig,
+	//...excludeBuild,
 };
