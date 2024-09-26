9f2e7c4f7b83c866f55442f0b7fc3213f8a81e85
Yun Jae Kim <82442974+yunjaekim00@users.noreply.github.com>
2024-09-26 16:55:40 +0900

    git reset

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
-import '../styles/globals.css';
-import Script from 'next/script';  // Import Script from Next.js
+import "../styles/globals.css";
+import Script from "next/script"; // Import Script from Next.js
 
 function MyApp({ Component, pageProps }) {
 	return (
@@ -10,7 +10,6 @@ function MyApp({ Component, pageProps }) {
 				data-website-id="acd1907e-3e2b-470d-bda6-8bd7b31819d6"
 				strategy="lazyOnload"
 			/>
-			{/* Your application */}
 			<Component {...pageProps} />
 		</>
 	);
