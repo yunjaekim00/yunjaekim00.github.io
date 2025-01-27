18d899536a3d3d6c51bfd86b842398183dda9bb6
Yun Jae Kim <82442974+yunjaekim00@users.noreply.github.com>
2025-01-27 16:18:24 +0900

    ga remove

--- pages/04.BackEnd/Rust/01.Intro/01-intro.md
+++ pages/04.BackEnd/Rust/01.Intro/01-intro.md
@@ -2,8 +2,11 @@
 title: History of Javascript 1
 date: 2025-01-27
 ---
+
 ## Introduction to Rust
+
 ### What is Rust
+
 ```ad-summary
 title:summary
 - Rust는 C, C++, Golang처럼 **Systems programming**을 위한 언어 중 하나이다.
@@ -11,8 +14,13 @@ title:summary
 ```
 
 ### Rust compiler
+
 ```ad-summary
 title:summary
 - **Compiler**는 소스코드를 컴퓨터가 이해할 수 있는 binary executable로 변환해주는 translator와 같은 것이다.
 - Rust는 다른 언어에 비해서도 특히 syntax가 strict하다.
 ```
+
+> [!info]
+> Here's a callout block.
+> It supports **Markdown**, [[Internal link|Wikilinks]], and [[Embed files|embeds]]!
--- pages/_app.js
+++ pages/_app.js
@@ -2,41 +2,21 @@ import "../styles/globals.css";
 //import { ThemeProvider } from "next-themes";
 import { useTheme } from "next-themes";
 import { useEffect } from "react";
-import Script from "next/script";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps, content }) {
 	const { theme, setTheme } = useTheme();
 	useEffect(() => {
 		setTheme("light");
-		// Initialize Google Analytics
-		window.dataLayer = window.dataLayer || [];
-		function gtag() {
-			window.dataLayer.push(arguments);
-		}
-		gtag("js", new Date());
-		gtag("config", "G-2Q3CM48GRW"); // Replace with your GA tracking ID
 	}, []);
 	return (
 		<>
-			{/*<!-- Google tag (gtag.js) -->*/}
-			<Script
-				strategy="afterInteractive"
-				src="https://www.googletagmanager.com/gtag/js?id=G-2Q3CM48GRW"
-			/>
-			<Script id="google-analytics" strategy="afterInteractive">
-				{`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-2Q3CM48GRW');
-        `}
-			</Script>
-
 			{/*<ThemeProvider>*/}
 			<Component {...pageProps} />
 			{/*</ThemeProvider>*/}
 		</>
 	);
 }
+
+
 
 export default MyApp;
